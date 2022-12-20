84a2ea6c528c28802d6f5c51002a6b9a31a70256
chiweenie6 <113393706+Chiweenie6@users.noreply.github.com>
2022-12-19 18:40:48 -0700

    so far for  first API

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,51 +5,51 @@
 
 var foodItem = document.getElementById("foodItem");
 
-function foodSearch() {
-    foodInput = foodItem.value;
-    console.log(foodInput);
+function foodSearch(food) {
+    food = foodItem.value;
+    console.log(food);
 
-    findFoodFacts(foodInput);
-    findRecipe(foodInput);
+    // findFoodFacts(food);
+    findRecipe(food);
 }
 
 
 
-// Fetching the information from API
-function findFoodFacts(food) {
+// // Fetching the information from API
+// function findFoodFacts(food) {
 
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '2baebc70a1mshd27e67c12d60db1p17859ejsn0d50f086b942',
-            'X-RapidAPI-Host': 'edamam-food-and-grocery-database.p.rapidapi.com'
-        }
-    };
+//     const options = {
+//         method: 'GET',
+//         headers: {
+//             'X-RapidAPI-Key': '2baebc70a1mshd27e67c12d60db1p17859ejsn0d50f086b942',
+//             'X-RapidAPI-Host': 'edamam-food-and-grocery-database.p.rapidapi.com'
+//         }
+//     };
 
-fetch('https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=' + food + '', options)
-	// .then(response => response.json())
-	// .then(response => console.log(response))
-	// .catch(err => console.error(err))
-    .then(function(response) {return response.json() })
-    .then(function(data) {
-        console.log(data);
-        foodInfo(data);
-    });
-}
+// fetch('https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=' + food + '', options)
+// 	// .then(response => response.json())
+// 	// .then(response => console.log(response))
+// 	// .catch(err => console.error(err))
+//     .then(function(response) {return response.json() })
+//     .then(function(data) {
+//         console.log(data);
+//         foodInfo(data);
+//     });
+// }
 
     
-// Place the fetched information into the web page
-function foodInfo (info) {
-    document.getElementById("CHOCDF").innerHTML = "Carbohydrate, by difference: " + info.hints[0].food.nutrients.CHOCDF + "g";
-    document.getElementById("ENERC_KCAL").innerHTML = "Energy: " + info.hints[0].food.nutrients.ENERC_KCAL + "kcal";
-    document.getElementById("FAT").innerHTML = " Total lipid (fat) content: " + (info.hints[0].food.nutrients.FAT) + "g";
-    document.getElementById("FIBTG").innerHTML = "Fiber, total dietary: " + info.hints[0].food.nutrients.FIBTG + "g";
-    document.getElementById("PROCNT").innerHTML = "Protein: " + info.hints[0].food.nutrients.PROCNT + "g";
+// // Place the fetched information into the web page
+// function foodInfo (info) {
+//     document.getElementById("CHOCDF").innerHTML = "Carbohydrate, by difference: " + info.hints[0].food.nutrients.CHOCDF + "g";
+//     document.getElementById("ENERC_KCAL").innerHTML = "Energy: " + info.hints[0].food.nutrients.ENERC_KCAL + "kcal";
+//     document.getElementById("FAT").innerHTML = " Total lipid (fat) content: " + (info.hints[0].food.nutrients.FAT) + "g";
+//     document.getElementById("FIBTG").innerHTML = "Fiber, total dietary: " + info.hints[0].food.nutrients.FIBTG + "g";
+//     document.getElementById("PROCNT").innerHTML = "Protein: " + info.hints[0].food.nutrients.PROCNT + "g";
 
 
-    var foodImg = document.getElementById("foodPicture");
-    foodImg.src = info.hints[0].food.image;
-}
+//     var foodImg = document.getElementById("foodPicture");
+//     foodImg.src = info.hints[0].food.image;
+// }
 
 
 
@@ -90,13 +90,28 @@ function getAPI(queryURL1) {
 
 // getAPI(queryURL1);
 
-
+// Putting the fetched information onto the page making it visible to the user.
 function firstAPIInfo (info) {
     var recipeImg = document.getElementById("recipePicture");
     recipeImg.src = info.hits[0].recipe.image;
 
     document.getElementById("label").innerHTML = "Dish Name: " + info.hits[0].recipe.label;
     document.getElementById("calories").innerHTML = "Calories: " + info.hits[0].recipe.calories;
+
+for (var i=0; i<5; i++) {    
+    // document.getElementById("digest").innerHTML = info.hits[i].recipe.digest[i].label;
+    console.log(info.hits[i].recipe.digest[i].label);
+
+    var digestion = (info.hits[i].recipe.digest[i].label) + ": " + (info.hits[i].recipe.digest[i].total);
+
+
+    var digestInfo = document.createElement("li");
+    var digestInfoBox = document.createTextNode(digestion);
+            digestInfo.appendChild(digestInfoBox);
+            document.getElementById("digest").appendChild(digestInfo);
+
+}
+    
     document.getElementById("dietLabels").innerHTML = "Diet Labels: " + toString(info.hits[0].dietLabels);
     document.getElementById("healthLabels").innerHTML = " Health Labels: " + info.hits[0].healthLabels;
 
@@ -107,19 +122,28 @@ function firstAPIInfo (info) {
 
 
     
-
-        for(let i=0; i<20; i++) {
+//  Placeing fetched info from an array onto the page for user to see.
+        for(let i=0; i<5; i++) {
             document.getElementById("arrayInfo").innerHTML = " Array: " + info.hits[i].recipe.label;
             
             var recipes = info.hits[i].recipe.label;
-            var recipeList = document.getElementById("arrayInfo").innerHTML;
-            
+            // var recipeList = document.getElementById("arrayInfo").innerHTML;
 
-            var myDiv = document.createElement('div');
-            myDiv.setAttribute("id", "myId");
-            myDiv.classList.add("myClass");
-            myDiv.textContent = recipes;
-            document.getElementById("arrayInfo").appendChild(myDiv);
+
+            var recipeButton = document.createElement("BUTTON");
+            var recipeButtonBox = document.createTextNode(recipes);
+            recipeButton.appendChild(recipeButtonBox);
+            document.getElementById("recipeList").appendChild(recipeButton);
+
+
+            recipeButton.onclick = function(recipe) {
+                recipe = recipes;
+
+                // findFoodFacts(recipe);
+                findRecipe(recipe);
+
+                console.log(recipes);
+            }
 
             console.log(info.hits[i].recipe.label);
     };
